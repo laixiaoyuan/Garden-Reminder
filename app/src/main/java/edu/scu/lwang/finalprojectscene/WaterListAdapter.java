d22e633b48c6f2d1be7f2cf705fdd681aaf899aa
mingming733 <jingyang.ming@gmail.com>
2016-05-24 18:18:51 -0700

    add class

---
+++ app/src/main/java/edu/scu/lwang/finalprojectscene/WaterListAdapter.java
@@ -0,0 +1,51 @@
+package edu.scu.lwang.finalprojectscene;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+/**
+ * Created by mingming on 5/9/16.
+ */
+public class WaterListAdapter extends CursorAdapter {
+
+    public WaterListAdapter(Context context, Cursor cursor, int flags) {
+        super(context, cursor, flags);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return LayoutInflater.from(context).inflate(R.layout.row, parent, false);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        String plantName = cursor.getString(cursor.getColumnIndex("PlantName"));
+        String imagePath = cursor.getString(cursor.getColumnIndex("PhotoPath"));
+        int fromNow = (cursor.getInt(cursor.getColumnIndex("NextWater")) - (int)Calendar.getInstance().getTimeInMillis()) / 86400000;
+        String waterDay;
+        if(fromNow == 0){
+            waterDay = "Today";
+        }else if(fromNow == 1){
+            waterDay = "Tomorrow";
+        }else{
+            waterDay = "After";
+        }
+        ((ImageView)view.findViewById(R.id.imageView2)).setImageURI(Uri.parse(imagePath));
+        TextView tvName = (TextView)view.findViewById(R.id.textView2);
+        tvName.setText(plantName);
+
+        TextView tvWater = (TextView)view.findViewById(R.id.textView3);
+        tvWater.setText(waterDay);
+
+//        BitmapFactory.Options optio
+    }
+}
