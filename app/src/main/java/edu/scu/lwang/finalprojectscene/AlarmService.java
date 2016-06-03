f200aca0d82954719ba2072f27d8aaa4ef0a6847
mingming733 <jingyang.ming@gmail.com>
2016-06-02 21:40:56 -0700

    add alarm service

---
+++ app/src/main/java/edu/scu/lwang/finalprojectscene/AlarmService.java
@@ -0,0 +1,61 @@
+package edu.scu.lwang.finalprojectscene;
+
+import android.app.AlarmManager;
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.Cursor;
+
+/**
+ * Created by mingming on 6/2/16.
+ */
+public class AlarmService extends IntentService {
+
+    public static final String CREATE = "CREATE";
+    public static final String CANCEL = "CANCEL";
+
+    private IntentFilter matcher;
+
+    public AlarmService() {
+        super("AlarmService");
+        matcher = new IntentFilter();
+        matcher.addAction(CREATE);
+        matcher.addAction(CANCEL);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        String action = intent.getAction();
+        String notificationId = intent.getStringExtra("notificationId");
+
+        if (matcher.matchAction(action)) {
+            execute(action, notificationId);
+        }
+    }
+
+    private void execute(String action, String notificationId) {
+        AlarmManager am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Cursor c = RemindMe.db.query(Notification.TABLE_NAME, null, "_id = ?",
+                new String[]{notificationId}, null, null, null);
+
+        if (c.moveToFirst()) {
+            Intent i = new Intent(this, AlarmReceiver.class);
+            i.putExtra("id", c.getLong(c.getColumnIndex(Notification.COL_ID)));
+            i.putExtra("msg", c.getString(c.getColumnIndex(Notification.COL_MSG)));
+
+            PendingIntent pi = PendingIntent.getBroadcast(this, 0, i,
+                    PendingIntent.FLAG_UPDATE_CURRENT);
+
+            long time = c.getLong(c.getColumnIndex(Notification.COL_DATETIME));
+            if (CREATE.equals(action)) {
+                am.set(AlarmManager.RTC_WAKEUP, time, pi);
+
+            } else if (CANCEL.equals(action)) {
+                am.cancel(pi);
+            }
+        }
+        c.close();
+    }
+
+}
