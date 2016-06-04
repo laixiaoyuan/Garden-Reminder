ceb2246d1d26dc71a4cc972539ed48c025159986
mingming733 <jingyang.ming@gmail.com>
2016-06-03 18:00:19 -0700

    add alarm receiver

--- app/src/main/java/edu/scu/lwang/finalprojectscene/AlarmService.java
+++ app/src/main/java/edu/scu/lwang/finalprojectscene/AlarmService.java
@@ -2,6 +2,7 @@ package edu.scu.lwang.finalprojectscene;
 
 import android.app.AlarmManager;
 import android.app.IntentService;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -14,6 +15,7 @@ public class AlarmService extends IntentService {
 
     public static final String CREATE = "CREATE";
     public static final String CANCEL = "CANCEL";
+    PlantDBHelper plantDB = new PlantDBHelper(this);
 
     private IntentFilter matcher;
 
@@ -27,27 +29,24 @@ public class AlarmService extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
         String action = intent.getAction();
-        String notificationId = intent.getStringExtra("notificationId");
+        int recID = intent.getIntExtra("recID",0);
 
         if (matcher.matchAction(action)) {
-            execute(action, notificationId);
+            execute(action, recID);
         }
     }
 
-    private void execute(String action, String notificationId) {
+    private void execute(String action, int recID) {
         AlarmManager am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-        Cursor c = RemindMe.db.query(Notification.TABLE_NAME, null, "_id = ?",
-                new String[]{notificationId}, null, null, null);
+        Cursor c = plantDB.getPlant(recID);
 
         if (c.moveToFirst()) {
             Intent i = new Intent(this, AlarmReceiver.class);
-            i.putExtra("id", c.getLong(c.getColumnIndex(Notification.COL_ID)));
-            i.putExtra("msg", c.getString(c.getColumnIndex(Notification.COL_MSG)));
 
             PendingIntent pi = PendingIntent.getBroadcast(this, 0, i,
                     PendingIntent.FLAG_UPDATE_CURRENT);
 
-            long time = c.getLong(c.getColumnIndex(Notification.COL_DATETIME));
+            long time = c.getLong(c.getColumnIndex("NextWater"));
             if (CREATE.equals(action)) {
                 am.set(AlarmManager.RTC_WAKEUP, time, pi);
 
