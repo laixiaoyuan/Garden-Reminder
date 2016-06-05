75903259a8beffc69290e95a5492332c70b459d2
mingming733 <jingyang.ming@gmail.com>
2016-06-05 16:24:34 -0700

    water it

--- app/src/main/java/edu/scu/lwang/finalprojectscene/PlantHistory.java
+++ app/src/main/java/edu/scu/lwang/finalprojectscene/PlantHistory.java
@@ -1,21 +1,13 @@
 package edu.scu.lwang.finalprojectscene;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -55,7 +47,6 @@ public class PlantHistory extends AppCompatActivity {
         gridview.setAdapter(new HistoryAdapter(this, plantHistoryName));
 
         System.out.println("got to the on create in PlantsGridView");
-<<<<<<< HEAD
 
 
         ImageButton home = (ImageButton)findViewById(R.id.home);
@@ -66,26 +57,7 @@ public class PlantHistory extends AppCompatActivity {
                 startActivity(new Intent(PlantHistory.this, PlatsGridView.class));
             }
         });
-=======
-        ActionBar actionBar = getSupportActionBar();
-//        actionBar.setTitle("ActionBar demo");
-//        actionBar.setSubtitle("this is a demo");
-//        actionBar.setBackgroundDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.action_bar_background));
-        actionBar.setIcon(R.mipmap.home);
 
-//        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setDisplayShowHomeEnabled(true);
-        actionBar.setHomeButtonEnabled(true);
-//Home button
-//        ImageButton button4 = (ImageButton) findViewById(R.id.button4);
-//
-//        button4.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v) {
-//                finish();
-//                startActivity(new Intent(WaterList.this, PlatsGridView.class));
-//            }
-//        });
->>>>>>> 3d8289bbb1d085333bf5ecf8bd826e0852c77364
 
 
         calendar = Calendar.getInstance();
